e938e1433127920c76069e7413df2ad0edb6e51c
NISHU SINGH <nishu0321singh@gmail.com>
2025-07-13 11:50:59 +0530

    Add new paragraph

---
+++ basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "mnl@google.com"
+var accountPassword = "549621"
+accountCity = "noida"
+let accountState;
+
+// accountId = 2 //not allowed
+
+accountEmail = "op@hu.com"
+accountPassword = "58421"
+accountCity = "jaipur"
+
+console.log(accountId);
+
+console.table([accountEmail, accountCity, accountPassword, accountState]);
